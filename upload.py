667d0b65f17e332d3a9e4a0d5001cf3252e5b959
Yatishhr <yatishhr@gmail.com>
2018-11-11 11:28:58 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,12 +3,12 @@ import json
 headers = {"Authorization": "Bearer ya29.GltRBuIZz0L_cmo6sUcHvoFYs9NEfV2PVPf8CCMUk1xdiqX6JV-tjJsBedBIFQqNfRmgq6xBqmXEyvzqDjlB92oFvuTMmAvSlErU-7yBgObtV_2QVvdTMJNkP4nj"
 } #put ur access token after the word 'Bearer '
 para = {
-    "name": "models.zip", #file name to be uploaded
+    "name": "models19.zip", #file name to be uploaded
     "parents": ["1r1CDFiE0xlcGrjledoFbLqK5rudZ8erf"] # make a folder on drive in which you want to upload files; then open that folder; the last thing in present url will be folder id
 }
 files = {
     'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
-    'file': ('application/zip',open("./models.zip", "rb")) # replace 'application/zip' by 'image/png' for png images; similarly 'image/jpeg' (also replace your file name)
+    'file': ('application/zip',open("./models19.zip", "rb")) # replace 'application/zip' by 'image/png' for png images; similarly 'image/jpeg' (also replace your file name)
 }
 r = requests.post(
     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
